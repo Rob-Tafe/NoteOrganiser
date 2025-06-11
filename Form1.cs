3e7b5d281846990c57c1bfe5f97db1deee8238ea
Rob-Tafe <p214430@tafe.wa.edu.au>
2025-06-11 15:30:13 +0800

    Updated functionality & layout

--- Form1.cs
+++ Form1.cs
@@ -15,34 +15,26 @@ namespace NoteOrganiser
 {
     public partial class NoteOrganiser : Form
     {
+
+
+
         public NoteOrganiser()
         {
             InitializeComponent();
         }
 
-        private void BtnOpen_Click(object sender, EventArgs e)
+
+        // This method generates a consistent file path forInitialDirectory
+        // to use. This method is called by BtnOpen and BtnSave.
+        private string GetFilePath()
         {
             string filePathRel = @"..\..\Notes Folder\";
             string filePathFull = Path.GetFullPath(Path.Combine(Application.StartupPath, filePathRel));
-            
-            OpenFileDialog openTxtFile = new OpenFileDialog();
-            openTxtFile.Title = "Open Text file";
-            openTxtFile.Filter = "Text files|*.txt";
-            openTxtFile.InitialDirectory = filePathFull;
 
-            if (openTxtFile.ShowDialog() == DialogResult.OK)
-            {
-                try
-                {
-                    ReadNote(openTxtFile.FileName);
-                }
-                catch
-                {
-                    MessageBox.Show("Error, unable to load file.", "Error");
-                    return;
-                }
-            }
+            return filePathFull;
         }
+
+
 
         private void ReadNote(string readFilePath)
         {
@@ -55,17 +47,71 @@ namespace NoteOrganiser
             }
         }
 
-        private void BtnSave_Click(object sender, EventArgs e)
-        {
 
+
+        private void BtnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openTxtFile = new OpenFileDialog();
+            openTxtFile.Title = "Open Text file";
+            openTxtFile.Filter = "Text files|*.txt";
+            openTxtFile.InitialDirectory = GetFilePath();
+
+            if (openTxtFile.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    ReadNote(openTxtFile.FileName);
+                }
+                catch
+                {
+                    MessageBox.Show("Error, unable to load file.", "Error (BtnOpen_Click Method)");
+                    return;
+                }
+            }
         }
+
+
 
         private void WriteNote(string writeFilePath)
         {
             string rtbText = RtbNoteMain.Text;
 
-            //using (StreamWriter writeFile = new StreamWriter(writeFilePath)
-            //{
-                
+            using (StreamWriter writeFile = new StreamWriter(writeFilePath))
+            {
+                writeFile.Write(rtbText, writeFilePath);
+            }
+        }
+
+
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveTxtFile = new SaveFileDialog();
+            saveTxtFile.Title = "Save Text file";
+            saveTxtFile.Filter = "Text files|*.txt";
+            saveTxtFile.InitialDirectory = GetFilePath();
+
+            saveTxtFile.FileName = "Note.txt";
+
+            if (saveTxtFile.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = saveTxtFile.FileName;
+                try
+                {
+                    WriteNote(saveTxtFile.FileName);
+                }
+                catch
+                {
+                    MessageBox.Show("Error, unable to save file.", "Error (BtnSave_Click Method)");
+                    return;
+                }
+            }
+        }
+
+
+
     }
+
+
+
 }
